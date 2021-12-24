0a15fd9041eafa31d9e66f992064d5871960baa6
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-12-24 13:43:23 +0300

    Create AzureOCR.py

---
+++ AzureOCR.py
@@ -0,0 +1,17 @@
+def AzureOCR():
+    path = input("Enter a URL For Analysis: ")
+    try: 
+        url = "https://microsoft-computer-vision3.p.rapidapi.com/detect"
+
+        payload = "{\r\n    \"url\": \""+path+"\"\r\n}"
+        headers = {
+    'content-type': "application/json",
+    'x-rapidapi-host': "microsoft-computer-vision3.p.rapidapi.com",
+    'x-rapidapi-key': "YOUR_RAPID_API_KEY"
+    }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+        print("Azure Computer Vision --> Success")
+        print(response.json())
+    except Exception as ex:
+        print(f"Azure Computer Vision Error --> {ex}")    
