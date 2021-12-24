27ca8bf887f5db6202fc59adb1202f62113dd88b
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-12-24 13:44:14 +0300

    Create AzureTextAI.py

---
+++ AzureTextAI.py
@@ -0,0 +1,34 @@
+def AzureTextAI():
+     
+     text = input("Enter a Sentence or Paragraph for text analsysis: ")
+     data = []
+     sending_text = ""
+     
+     if text:
+         data = text.split(' ')
+         if len(data) > 0:
+             for i in range(len(data)):
+                 sending_text += data[i]
+         else:
+             sending_text = text
+         try:
+             
+            url = "https://microsoft-content-moderator2.p.rapidapi.com/ProcessText/Screen"
+            querystring = {"PII":"true","autocorrect":"true","classify":"true"}
+
+            payload = text
+            headers = {
+    'content-type': "text/plain",
+    'x-rapidapi-host': "microsoft-content-moderator2.p.rapidapi.com",
+    'x-rapidapi-key': "YOUR_RAPID_API_KEY"
+    }
+
+            response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+            print("Azure Computer Vision --> Success")
+            print(response.json()['PII'])
+         except Exception as ex:
+            no_pii = f"{ex}"
+            if 'PII' in no_pii:
+                print("Azure Computer Vision --> No 'PII' Data")
+            else:
+                print(f"Azure Computer Vision Error --> {no_pii}") 
